771ab16383fe98f85ebcc90e20068e64d2c33731
giovanniattolico <franciscopperfield8@gmail.com>
2024-10-08 09:52:54 +0200

    base completata

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,38 @@
-function generateRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function generateRandomInteger(min = 0,  max = 100){
+    let number = Math.random()
+    number = number * (max - min) + min
+    return Math.floor(number)
 }
 
-const numeroSegreto = generateRandom(1, 100);
+const numeroSegreto = generateRandomInteger(0, 100);
 let tentativi = 5;
 
-while (tentativi > 0) {
-    const inputUtente = prompt("Indovina il numero (1-100):");
-    const numeroUtente = parseInt(inputUtente);
+let btn = document.querySelector("#guessBTN")
 
+function handleClick(event){
+    console.log(event)
+
+    const inputUtente = document.querySelector("#guess")
+    const numeroUtente = parseInt(inputUtente.value)
+    
     if (isNaN(numeroUtente) || numeroUtente < 1 || numeroUtente > 100) {
         console.log("Inserisci un numero da 1 a 100.");
-        continue;
     }
-
+    
     if (numeroUtente > numeroSegreto) {
         console.log("Troppo grande");
     } else if (numeroUtente < numeroSegreto) {
-        console.log("Troppo piccolo");
+         console.log("Troppo piccolo");
     } else if (numeroUtente === numeroSegreto) {
         console.log("Sei Cane, hai vinto!");
-        break;
     }
-
+    
     tentativi--;
     console.log(`Tentativi rimasti: ${tentativi}`);
+    
+    if (tentativi === 0) {
+        console.log(`Hai perso! Il numero segreto era ${numeroSegreto}.`);
+    }
 }
 
-if (tentativi === 0) {
-    console.log(`Hai perso! Il numero segreto era ${numeroSegreto}.`);
-}
+btn.addEventListener("click", handleClick)
